e5570a0264e195809e3a38bc1c531094aed674a4
Yusuf Çırak <yusuf10901@hotmail.com>
2023-06-22 22:59:03 +0300

    Domain with entities

---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Common;
+public abstract class BaseEntity
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+}
