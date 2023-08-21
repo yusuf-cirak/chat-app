29bd4fd462734487213595366beeb272a466183c
Yusuf Çırak <yusuf10901@hotmail.com>
2023-08-22 00:53:09 +0300

    chat improvements

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       colors: {
         "wp-blue": "#00a884",
         "wp-gray": "#d1d7db",
+        "wp-panel-background": "#F0F2F5",
+        "wp-panel-r-border": "#d2d7db",
+        "wp-panel-b-border": "#e9edef",
       },
     },
   },
