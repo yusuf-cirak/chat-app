0c1aa24e20ddd54322eb7e3135a5a999761a2d26
Yusuf Çırak <yusuf10901@hotmail.com>
2023-08-25 00:22:33 +0300

    Chat users added

--- frontend/src/app/core/layouts/chat/chat.component.ts
+++ frontend/src/app/core/layouts/chat/chat.component.ts
@@ -1,11 +1,135 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface SidebarChatUser {
+  profilePicture: string;
+  name: string;
+  lastMessage: string;
+  lastMessageTime: string;
+  unreadMessages: number;
+}
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor, FormsModule],
   templateUrl: './chat.component.html',
 })
-export class ChatComponent {}
+export class ChatComponent {
+  sideBarChatUsers: SidebarChatUser[] = [];
+  searchInput = '';
+
+  constructor() {
+    this.sideBarChatUsers = [
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '5 min ago',
+        unreadMessages: 2,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'Jane Doe',
+        lastMessage: 'Can we meet today?',
+        lastMessageTime: '10 min ago',
+        unreadMessages: 1,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: "Don't forget to call me.",
+        lastMessageTime: '15 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'Jane Doe',
+        lastMessage: 'Good job!',
+        lastMessageTime: '20 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '25 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'Jane Doe',
+        lastMessage: 'Can we meet today?',
+        lastMessageTime: '30 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: "Don't forget to call me.",
+        lastMessageTime: '35 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'Jane Doe',
+        lastMessage: 'Good job!',
+        lastMessageTime: '40 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'John Doe',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+      {
+        profilePicture: 'assets/images/noPic.svg',
+        name: 'Yusuf',
+        lastMessage: 'Hey, how are you?',
+        lastMessageTime: '45 min ago',
+        unreadMessages: 0,
+      },
+    ];
+  }
+
+  ngOnInit() {}
+}
