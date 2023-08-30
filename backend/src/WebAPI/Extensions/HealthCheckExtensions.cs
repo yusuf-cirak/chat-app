54cc14b850816f7c173cd23130e9f7f80c1d2e04
Yusuf Çırak <yusuf10901@hotmail.com>
2023-08-30 22:04:39 +0300

    Refactor for WebAPI, JWT Authentication

---
+++ backend/src/WebAPI/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Extensions;
+
+internal static class HealthCheckExtensions
+{
+    internal static void AddHealthCheckServices(this IServiceCollection services,IConfiguration configuration)
+    {
+        var mongoDbConnStr = configuration.GetSection("MongoDbSettings:ConnectionURI").Get<string>();
+        services.AddHealthChecks().AddMongoDb(mongoDbConnStr!,name:"MongoDB");
+    }
+}
--- backend/src/WebAPI/Program.cs
+++ backend/src/WebAPI/Program.cs
@@ -1,43 +1,33 @@
-using System.IO.Compression;
 using Application;
 using HealthChecks.UI.Client;
 using Infrastructure;
 using Infrastructure.Constants;
 using Infrastructure.SignalR.Hubs;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.ResponseCompression;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddApplicationServices();
-var mongoDbConnStr = builder.Configuration.GetSection("MongoDbSettings:ConnectionURI").Get<string>();
-builder.Services.AddHealthChecks().AddMongoDb(mongoDbConnStr!,name:"MongoDB");
+builder.Services.AddResponseCompressionServices(); // From WebAPI\Extensions\ResponseCompressionExtensions.cs
 
+builder.Services.AddHealthCheckServices(builder.Configuration); // From WebAPI\Extensions\HealthCheckExtensions.cs
 
-builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices(); // From Application\ServiceRegistration.cs
+
+builder.Services.AddInfrastructureServices(builder.Configuration); // From Infrastructure\ServiceRegistration.cs
+
+builder.Services.AddJwtAuthenticationServices(builder.Configuration); // From WebAPI\Extensions\JwtBearerExtensions.cs
+
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddResponseCompression(options =>
-{
-    options.EnableForHttps = true;
-    options.Providers.Add<BrotliCompressionProvider>();
-    options.Providers.Add<GzipCompressionProvider>();
-});
-
-builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
-{
-    options.Level = CompressionLevel.Fastest;
-});
-
-builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-{
-    options.Level = CompressionLevel.Fastest;
-});
+builder.Services.AddSwaggerGenServices(); // From WebAPI\Extensions\SwaggerExtensions.cs
 
 var app = builder.Build();
 
@@ -58,6 +48,8 @@ app.UseHealthChecks("/_health", new HealthCheckOptions
     
 }
 ); // RequireAuthorization, RequireCors, RequireHost possible for limiting calls for /_health endpoint.
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
