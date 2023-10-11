a054ac98c4b978ceb9b79679736891e6e7e0bb8c
Yusuf Çırak <yusuf10901@hotmail.com>
2023-10-11 21:54:56 +0300

    Docker compose, start.sh, actions update

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+export MONGO_USERNAME="${MONGO_USERNAME:-admin}"
+export MONGO_PASSWORD="${MONGO_PASSWORD:-admin}"
+export MONGO_PORTS="${MONGO_PORTS:-27017:27017}"
+
+export ASPNETCORE_ENVIRONMENT="${ASPNETCORE_ENVIRONMENT:-Development}"
+
 docker compose up --force-recreate --build -d && docker container restart nginx
