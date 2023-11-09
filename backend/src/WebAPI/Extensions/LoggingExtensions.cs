dbc2ee872b0e762b6b6be81167907d8e8d579435
Yusuf Çırak <yusuf10901@hotmail.com>
2023-11-09 18:37:46 +0300

    asynchronous logging

--- backend/src/WebAPI/Extensions/LoggingExtensions.cs
+++ backend/src/WebAPI/Extensions/LoggingExtensions.cs
@@ -12,20 +12,21 @@ public static class LoggingExtensions
     {
         var elasticConfig = builder.Configuration.GetSection("ElasticSearchConfig").Get<ElasticSearchConfig>() ??
                             throw new Exception("ElasticSearchConfig is null");
-        
+
         var logger = new LoggerConfiguration()
             .Enrich.FromLogContext()
-            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticConfig.ConnectionString))
-            {
-                IndexFormat = $"log-{DateTime.UtcNow:yyyy-MM}",
-                AutoRegisterTemplate = true,
-                MinimumLogEventLevel = LogEventLevel.Information,
-                CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
-                NumberOfShards = 1,
-                NumberOfReplicas = 1,
-                ModifyConnectionSettings = connectionConfiguration => connectionConfiguration
-                    .BasicAuthentication(elasticConfig.UserName, elasticConfig.Password)
-            })
+            .WriteTo.Async(log => log.Elasticsearch(
+                new ElasticsearchSinkOptions(new Uri(elasticConfig.ConnectionString))
+                {
+                    IndexFormat = $"log-{DateTime.UtcNow:yyyy-MM}",
+                    AutoRegisterTemplate = true,
+                    MinimumLogEventLevel = LogEventLevel.Information,
+                    CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
+                    NumberOfShards = 1,
+                    NumberOfReplicas = 1,
+                    ModifyConnectionSettings = connectionConfiguration => connectionConfiguration
+                        .BasicAuthentication(elasticConfig.UserName, elasticConfig.Password)
+                }))
             .CreateLogger();
 
         builder.Host.UseSerilog(logger);
